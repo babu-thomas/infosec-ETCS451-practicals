1d14b6322d9609bced4e07c1beec75ace37ad054
Babu Thomas <babuthomas138@gmail.com>
2017-11-14 01:42:01 +0530

    [Playfair] Get position in key table

--- playfair_cipher.cpp
+++ playfair_cipher.cpp
@@ -1,10 +1,16 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
 #include <string>
-#include <array>
 #include <unordered_map>
+#include <utility>
 
 std::string encrypt(const std::string& message, const std::string& key);
 std::array<char, 25> gen_key_table(const std::string& key);
+void print_key_table(const std::array<char, 25>& key_table);
+std::pair<int, int> find_pos(char letter, const std::array<char, 25>& key_table);
+int idx(int row, int col);
+std::pair<int, int> idx(int i);
 
 int main()
 {
@@ -22,11 +28,25 @@ int main()
 std::string encrypt(const std::string& message, const std::string& key)
 {
 	std::array<char, 25> key_table = gen_key_table(key);
+	std::cout << "\nKey table: " << "\n";
+	print_key_table(key_table);
 
 	std::string plaintext = message.size() % 2 == 0 ? message : message + 'Z';
-	std::cout << plaintext;
+	std::cout << "\nPlaintext: " << plaintext << "\n";
 
-	std::string ciphertext;
+	std::string ciphertext = plaintext;
+	for(auto i = 0u; i <= plaintext.size() - 2; i += 2)
+	{
+		char letter1 = plaintext[i];
+		char letter2 = plaintext[i + 1];
+		std::pair<int, int> letter1_pos = find_pos(letter1, key_table);
+		std::pair<int, int> letter2_pos = find_pos(letter2, key_table);
+		std::cout << letter1 << " pos: " << letter1_pos.first << ", " << letter1_pos.second << "\n";
+		std::cout << letter2 << " pos: " << letter2_pos.first << ", " << letter2_pos.second << "\n";
+		// Same column
+		// Same row
+		// Rectangle
+	}
 	return ciphertext;
 }
 
@@ -58,4 +78,35 @@ std::array<char, 25> gen_key_table(const std::string& key)
 		}
 	}
 	return key_table;
+}
+
+void print_key_table(const std::array<char, 25>& key_table)
+{
+	for(int i = 0; i < 5; i++)
+	{
+		std::cout << key_table[idx(i, 0)];
+		for(int j = 1; j < 5; j++)
+		{
+			std::cout << " " << key_table[idx(i, j)];
+		}
+		std::cout << "\n";
+	}
+}
+
+std::pair<int, int> find_pos(char letter, const std::array<char, 25>& key_table)
+{
+	int i = std::find(key_table.begin(), key_table.end(), letter) - key_table.begin();
+	return idx(i);
+}
+
+int idx(int row, int col)
+{
+	if(row == 5) row = 0;
+	if(col == 5) col = 0;
+	return row * 5 + col;
+}
+
+std::pair<int, int> idx(int i)
+{
+	return std::make_pair(i / 5, i % 5);
 }
